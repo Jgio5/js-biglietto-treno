4ec4dd1b44e213140d89d56a2e72f7d58e24d1bf
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-02-23 16:24:44 +0100

    Esercizio Completato
    - blocco utoniti
    -  un po' di css per non dimenticarsi

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 const costoKm = 0.21;
-const costoKmsconto20 = 0.21 - (0.21 * 0.2);
-const costoKmsconto40 = 0.21 - (0.21 * 0.4);
+const costoKmsconto20 = 0.21 - (0.21 * 0.2); //sconto 20% minorenni
+const costoKmsconto40 = 0.21 - (0.21 * 0.4); //sconto 40% over 65
 
 var etaPasseggero = prompt("Inserisci la tua età");
 console.log(etaPasseggero);
+document.getElementById('age').innerHTML = etaPasseggero;
 
 var kmTotali = prompt("Inserisci i km del tuo viaggio");
 console.log(kmTotali);
+document.getElementById('km').innerHTML = kmTotali;
 
+var prezzoKmStandard = (Math.round((kmTotali * costoKm) * 100)/100).toFixed(2);
+var prezzoKmYoung = (Math.round(kmTotali * costoKmsconto20 * 100)/100).toFixed(2);
+var prezzoKmSilver = (Math.round(kmTotali * costoKmsconto40 * 100)/100).toFixed(2);
 
-if (etaPasseggero <= 17) {
-  alert("Il prezzo del biglietto è di " + (Math.round(kmTotali * costoKmsconto20 * 100)/100).toFixed(2) + " euro");
+//verifica Input numerici
+if ( (isNaN(etaPasseggero)) && (isNaN(kmTotali)) ) {
+  alert("Inserisci SOLO valori numerici");
 }
-else if (etaPasseggero >= 65) {
-  alert("Il prezzo del biglietto è di " + (Math.round(kmTotali * costoKmsconto40 * 100)/100).toFixed(2) + " euro");
+else if ( isNaN(etaPasseggero) ) {
+  alert("Inserisci un valore numerico per l'età");
 }
-else {
-  alert("Il prezzo del biglietto è di " + kmTotali * costoKm + " euro");
+else if ( isNaN(kmTotali) ) {
+  alert("Inserisci un valore numerico per i km");
+}
+// Input numerici inseriti
+  else {
+  if (etaPasseggero <= 17) {
+    document.getElementById('price').innerHTML =  prezzoKmYoung;
+    document.getElementById('sconto').innerHTML = "Sconto Young 20%";
+  }
+  else if (etaPasseggero >= 65) {
+    document.getElementById('price').innerHTML = prezzoKmSilver;
+    document.getElementById('sconto').innerHTML = "Sconto Silver 40%";
+  }
+  else {
+    document.getElementById('price').innerHTML = prezzoKmStandard;
+    document.getElementById('sconto').innerHTML = "Non sono stati applicati degli sconti";
+  }
 }
